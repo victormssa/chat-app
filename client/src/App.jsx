fe16f821d898ab02c3722cb41dc4fde156eb8eee
victormssa <victormssalves@outlook.com>
2022-06-29 03:00:46 -0300

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
+import "./App.css";
 
 import { ChannelListContainer, ChannelContainer } from "./components";
 
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { ChannelList, useChatContext } from "stream-chat-react";
+import Cookies from 'universal-cookie';
 
-export const ChannelListContainer = () => {
-  return ( 
-    <div>
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import ChatIcon from "../assets/chat.png";
+import LogoutIcon from "../assets/logout.png";
 
+const SideBar = () => (
+    <div className="channel-list__sidebar">
+        <div className="channel-list__sidebar__icon1">
+            <div className="icon1__inner">
+                <img src={ChatIcon} alt="Icon" width="30" />
+            </div>
+        </div>
+        <div className="channel-list__sidebar__icon2">
+            <div className="icon1__inner">
+                <img src={LogoutIcon} alt="Logout" width="30" />
+            </div>
+        </div>
     </div>
+);
+
+const CompanyHeader = () => (
+    <div className="channel-list__header">
+        <p className="channel-list__header__text">MyChat</p>
+    </div>
+);
+
+const ChannelListContainer = () => {
+  return ( 
+    <>
+        <SideBar/>
+        <div className="channel-list__list__wrapper">
+            <CompanyHeader />
+            <ChannelSearch />
+        </div>
+    </>
   );
 };
 export default ChannelListContainer;
